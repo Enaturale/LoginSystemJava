6582dd59698237457cdab7e0cced01971b946ae5
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-08-30 14:53:58 +0100

    Login System Using Java

--- src/LoginPage.java
+++ src/LoginPage.java
@@ -6,8 +6,7 @@ import javax.swing.JTextField;
 import javax.swing.JPasswordField;
 import javax.swing.JLabel;
 
-
-
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -34,15 +33,32 @@ public class LoginPage implements ActionListener{
         idLabel.setBounds(50, 100, 75, 25);
         passLabel.setBounds(50, 150, 75, 25);
 
+        userID.setBounds(125, 100, 200, 25);
+        userPassword.setBounds(125, 150, 200, 25);
+
+        login.setBounds(125, 200, 100, 25);
+        login.addActionListener(this);
+
+        reset.setBounds(225, 200, 100, 25);
+        reset.addActionListener(this);
+
+
+        messageLabel.setBounds(125, 250, 250, 35);
+        messageLabel.setFont(new  Font(null, Font.BOLD, 30));
+
         frame.add(idLabel);
         frame.add(passLabel);
-
-
+        frame.add(messageLabel);
+        frame.add(userID);
+        frame.add(userPassword);
+        frame.add(login);
+        frame.add(reset);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setTitle("Login Page");
         frame.setSize(400, 400);
         frame.setLayout(null);
+        frame.setLocation(400, 180);
         frame.setVisible(true);
 
 
