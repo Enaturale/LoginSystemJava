95058fa349efa4c547fee5fed18778933c8019c1
The Real Tech Lady <bukolaarowosebi@gmail.com>
2021-09-06 14:16:59 +0100

    Image icon added to label

--- src/WelcomePage.java
+++ src/WelcomePage.java
@@ -6,24 +6,24 @@ import javax.swing.*;
 public class WelcomePage implements ActionListener {
     JFrame frame = new JFrame("Welcome Page");
     JLabel welcomelabel = new JLabel("Hello!");
+    JLabel HelloLabel = new JLabel("Image Placeholder");
+    ImageIcon image = new ImageIcon();
 
     WelcomePage(String userId){
+        //adding a menu bar
         JMenuBar menuBar = new JMenuBar();
         JMenu home = new JMenu("Home");
         JMenu  accounts = new JMenu("Accounts");
         JMenu  profile = new JMenu("Profile");
-        JMenu exit = new JMenu("Exit");
+        // JMenu exit = new JMenu("Exit");
 
-        exit.addActionListener(new ActionListener(){
-
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                // TODO Auto-generated method stub
-                System.exit(1);
-                
-            }
-            
-        });
+        // exit.addActionListener(new ActionListener(){
+        //     @Override
+        //     public void actionPerformed(ActionEvent e) {
+        //         // TODO Auto-generated method stub
+        //         frame.dispose();                
+        //     }            
+        // });
 
         menuBar.setBounds(0, 0, 800, 30);
         //menuBar.setBackground(Color.blue);
@@ -33,13 +33,29 @@ public class WelcomePage implements ActionListener {
         menuBar.add(home);
         menuBar.add(accounts);
         menuBar.add(profile);
-        menuBar.add(exit);
+        //menuBar.add(exit);
 
+        //adding images and Label
+        JPanel panel = new JPanel();
+        panel.setBounds(20, 50, 200, 100);
+
+        
+        // ImageIcon image = new ImageIcon("Spark8.jpeg");
+        // label.setIcon(image);     
+        
+
+
+        HelloLabel.setBounds(20, 100, 400,100);
+        HelloLabel.setIcon(new ImageIcon("src/Spark8.jpeg"));
+
+
+        //adding all components to the frame
         frame.add(menuBar);
+        frame.add(HelloLabel);
 
-        welcomelabel.setBounds(20, 100, 200, 40);
-        welcomelabel.setFont(new Font(null, Font.ITALIC, 20));
-        welcomelabel.setText("Hello " +  userId);
+        // welcomelabel.setBounds(20, 100, 200, 40);
+        // welcomelabel.setFont(new Font(null, Font.ITALIC, 20));
+        // welcomelabel.setText("Hello " +  userId);
 
 
         frame.add(welcomelabel);
